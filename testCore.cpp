86130b93814c4eb877f2dd1108476acd1c64748d
jianping.xia <jianping.xia@juphoon.com.cn>
2018-06-01 17:21:03 +0800

    TestGit aaa

---
+++ testCore.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>  
+#include<iostream>
+using namespace std;
+
+class Person
+{
+	public:
+	int age;
+	int salary;
+	
+};
+
+int main()  
+{  
+   // char *ptr="linuxers.cn";  
+    //*ptr=0;  
+	
+	Person * p = 0;
+	p->age = 45;
+	
+ return 0;
+} 
