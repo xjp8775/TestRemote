82cdce956534730bcb849ecc2255ec1cce7cb0a9
jianping.xia <jianping.xia@justalk.com.cn>
2018-09-12 10:14:18 +0800

    Test3 0912

--- testCore.cpp
+++ testCore.cpp
@@ -47,7 +47,6 @@ void Test0912Func()
 
 
 
-
 int main()  
 {  
    // char *ptr="linuxers.cn";  
@@ -62,11 +61,13 @@ int main()
 
     //this is a second branch
     
-	int kkk = 0;
-
 	Person * p = 0;
 	p->age = 45;
 	int vari0607 = 50;
+	
+	//0912 3 rd
+	int dd = 0；
+	
 	
  return 0;
  
